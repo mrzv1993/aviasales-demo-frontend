0efc013095e62dd02f36a0eb181259b3203200fd
mrzvmrzvmrzv <xxmrzv@gmail.com>
2018-02-17 23:17:42 +1000

    feat: MarketPlaces fixed

--- src/Footer/Marketplace.js
+++ src/Footer/Marketplace.js
@@ -4,7 +4,7 @@ import appStore from "../assets/app-store.svg";
 import googlePlay from "../assets/google-play.svg";
 import windowsPhone from "../assets/windows-phone.svg";
 
-const MarketPlaces = styled.ul`
+const MarketPlaces = styled.div`
   display: flex;
   flex-wrap: wrap;
   margin: 0 auto;
@@ -15,36 +15,26 @@ const MarketPlaces = styled.ul`
   }
 `;
 
-const Store = styled.li`
-  padding: 0.25rem 0;
-
-  @media (min-width: 767px) {
-    padding: 0 0.25rem;
-  }
+const StoreImg = styled.img`
+  margin-left: 0.5rem;
 `;
 
-const AppStore = styled.img``;
-const GooglePlay = styled.img``;
-const WindowsPhone = styled.img``;
+const AppStore = StoreImg.extend``;
+const GooglePlay = StoreImg.extend``;
+const WindowsPhone = StoreImg.extend``;
 
 export default function() {
   return (
     <MarketPlaces>
-      <Store>
-        <a href="#">
-          <AppStore src={appStore} alt="Aviasales" title="Aviasales" />
-        </a>
-      </Store>
-      <Store>
-        <a href="#">
-          <GooglePlay src={googlePlay} alt="Aviasales" title="Aviasales" />
-        </a>
-      </Store>
-      <Store>
-        <a href="#">
-          <WindowsPhone src={windowsPhone} alt="Aviasales" title="Aviasales" />
-        </a>
-      </Store>
+      <a href="#">
+        <AppStore src={appStore} alt="Aviasales" title="Aviasales" />
+      </a>
+      <a href="#">
+        <GooglePlay src={googlePlay} alt="Aviasales" title="Aviasales" />
+      </a>
+      <a href="#">
+        <WindowsPhone src={windowsPhone} alt="Aviasales" title="Aviasales" />
+      </a>
     </MarketPlaces>
   );
 }
